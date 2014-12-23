aa1aea5a3eb65f9178cb6b2d4396e40d8de3d654
Matt Nish <matt.nish@gmail.com>
2014-12-22 19:30:53 -0800

    Updated throttling and added starting pixel to begin animation

--- simple-parallax.js
+++ simple-parallax.js
@@ -14,6 +14,8 @@
   var ParallaxScroll = function ParallaxScroll(element, options){
     this.$el        = $(element);
     this.rate       = options.rate || .5;
+    this.max        = options.max || 0;  // maximum pixel distance to translate the element
+    this.starting   = options.starting || 0;  // pixel distance from top to start animation
     if(options.opacity){
       this.opacity        = true;
       this.opacitySpread  = options.opacitySpread || 500;
@@ -25,31 +27,42 @@
       var $window = $(window);
       if(this.opacity){
         $window.scroll(function() {
-          $.throttle(this.scrollWithOpacity(), 100);
+          $.throttle(150, false, this.scrollWithOpacity(), true);
         }.bind(this));
       } else {
         $window.scroll(function() {
-          $.throttle(this.scroll(), 100);
+          $.throttle(150, false, this.scroll(), true);
         }.bind(this));
       }
     },
     scroll: function(){
       var yOffset = window.pageYOffset,
-          val     = yOffset*this.rate;
-      this.$el.css({
+          val     = (yOffset - this.starting)*this.rate;
+      if((this.max === 0) || (Math.abs(this.max) > val) && (yOffset > this.starting)){
+        this.$el.css({
           '-webkit-transform':'translate3d(0,'+val+'px,0)',
           '-moz-transform':'translate3d(0,'+val+'px,0)',
           '-o-transform':'translate3d(0,'+val+'px,0)',
           '-ms-transform':'translate3d(0,'+val+'px,0)',
           'transform':'translate3d(0,'+val+'px,0)'
-      });
+        });
+      } else {
+        this.$el.css({
+          '-webkit-transform':'translate3d(0,0,0)',
+          '-moz-transform':'translate3d(0,0,0)',
+          '-o-transform':'translate3d(0,0,0)',
+          '-ms-transform':'translate3d(0,0,0)',
+          'transform':'translate3d(0,0,0)'
+        });
+      }
     },
     scrollWithOpacity: function(){
       var yOffset = window.pageYOffset,
-          val     = yOffset*this.rate,
+          val     = (yOffset - this.starting)*this.rate,
           opacity = 1 - (yOffset/this.opacitySpread),
           opacity = (opacity < 0) ? 0 : opacity;
-      this.$el.css({
+      if((this.max === 0) || (Math.abs(this.max) > val) && (yOffset > this.starting)){
+        this.$el.css({
           '-webkit-transform':'translate3d(0,'+val+'px,0)',
           '-moz-transform':'translate3d(0,'+val+'px,0)',
           '-o-transform':'translate3d(0,'+val+'px,0)',
@@ -58,7 +71,19 @@
           '-moz-opacity': opacity,
           '-khtml-opacity': opacity,
           'opacity': opacity
-      });
+        });
+      } else {
+        this.$el.css({
+          '-webkit-transform':'translate3d(0,0,0)',
+          '-moz-transform':'translate3d(0,0,0)',
+          '-o-transform':'translate3d(0,0,0)',
+          '-ms-transform':'translate3d(0,0,0)',
+          'transform':'translate3d(0,0,0)',
+          '-moz-opacity': 1,
+          '-khtml-opacity': 1,
+          'opacity': 1
+        });
+      }
     }
   };
 })(jQuery, window, document);
